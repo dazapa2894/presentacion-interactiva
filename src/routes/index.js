b04d482dd87a15d251ea359440b8c079a53e3039
David <dazapa2894@gmail.com>
2020-07-26 14:00:14 -0500

    creando subcarpetas y organizando el proyecto

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,28 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/', (req, res) => {
-  res.render('index', {
-    titulo: "Presentaciones",
-    style_name: "index_style",
-    video_controls: "",
-    votante: true,
-    script_name: "index_js"
-  });
-})
 
-router.get('/control', (req, res) => {
-  res.render('control', {
-    titulo: "Controlador de presentaciones",
-    style_name: "control_style",
-    video_controls: "controls",
-    controlador: true,
-    script_name: "control_js"
-  })
-});
-
-// lo que estoy intentando es separar las diapositivas en carpetas 
-// pero como que no detectan los estilos ni los videos o algo asi :(
 router.get('/empresarial/', (req, res) => {
   res.render('index', {
     titulo: "Presentaciones",
@@ -31,7 +10,8 @@ router.get('/empresarial/', (req, res) => {
     subfolder: "empresarial",
     video_controls: "",
     votante: true,
-    script_name: "index_js"
+    script_name: "index_js",
+    empresarial: true
   });
 })
 
@@ -42,10 +22,87 @@ router.get('/empresarial/control', (req, res) => {
     subfolder: "empresarial",
     video_controls: "controls",
     controlador: true,
-    script_name: "control_js"
+    script_name: "control_js",
+    empresarial: true
   })
 });
 
 
-
+// lo que estoy intentando es separar las diapositivas en carpetas 
+// pero como que no detectan los estilos ni los videos o algo asi :(
+  router.get('/politico-d/', (req, res) => {
+    res.render('index', {
+      titulo: "Presentaciones",
+      style_name: "index_style",
+      subfolder: "politico-d",
+      video_controls: "",
+      votante: true,
+      script_name: "index_js",
+      politico_d: true
+    });
+  })
+  
+  router.get('/politico-d/control', (req, res) => {
+    res.render('control', {
+      titulo: "Controlador de presentaciones",
+      style_name: "control_style",
+      subfolder: "politico-d",
+      video_controls: "controls",
+      controlador: true,
+      script_name: "control_js",
+      politico_d: true
+    })
+  });
+  
+  // lo que estoy intentando es separar las diapositivas en carpetas 
+  // pero como que no detectan los estilos ni los videos o algo asi :(
+    router.get('/politico-i/', (req, res) => {
+      res.render('index', {
+        titulo: "Presentaciones",
+        style_name: "index_style",
+        subfolder: "politico-i",
+        video_controls: "",
+        votante: true,
+        script_name: "index_js",
+        politico_i: true
+      });
+    })
+    
+    router.get('/politico-i/control', (req, res) => {
+      res.render('control', {
+        titulo: "Controlador de presentaciones",
+        style_name: "control_style",
+        subfolder: "politico-i",
+        video_controls: "controls",
+        controlador: true,
+        script_name: "control_js",
+        politico_i: true
+      })
+    });
+    
+    
+    
+    // presentacion - v1
+    // router.get('/', (req, res) => {
+    //   res.render('index', {
+    //     titulo: "Presentaciones",
+    //     style_name: "index_style",
+    //     video_controls: "",
+    //     votante: true,
+    //     script_name: "index_js"
+    //   });
+    // })
+    
+    // router.get('/control', (req, res) => {
+    //   res.render('control', {
+    //     titulo: "Controlador de presentaciones",
+    //     style_name: "control_style",
+    //     video_controls: "controls",
+    //     controlador: true,
+    //     script_name: "control_js"
+    //   })
+    // });
+    
+    
+    
 module.exports = router;
